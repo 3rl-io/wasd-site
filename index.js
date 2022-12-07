59953c4f45fc18ed68ee253957367628fc66ea49
kiran <kiranmkota@gmail.com>
2022-12-06 20:42:36 -0800

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express'), app =  express();
+app.use("/", express.static(__dirname + '/public', {maxAge: "3d"}));
+app.listen(8000, () => {
+    console.log(`listening on port 8000`)
+});
