524115e5a8145eddd2d16f32d29902c6ea31db6b
kiran <kiranmkota@gmail.com>
2023-12-27 13:59:36 -0800

    threejs v 155>160 follow-camera broke use cdn alpine update licenses

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express'), app =  express();
 app
-    .use("/alpine.min.js", express.static('node_modules/alpinejs/dist/cdn.js'))
     .use("/", express.static(__dirname + '/public', {maxAge: "3d"}));
 
 app.listen(8000, () => {
